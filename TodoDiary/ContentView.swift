2c39d57f9cc4998ea4177074d47b4cb1d82a98ac
Yu357 <forestgreen14689@gmail.com>
2021-09-15 14:57:50 +0900

    Initial Commit

---
+++ TodoDiary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoDiary
+//
+//  Created by Yu on 2021/09/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
