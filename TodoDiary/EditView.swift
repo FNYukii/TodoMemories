a35f97d36b507d903aee376e3581b51b07858e5d
Yu357 <forestgreen14689@gmail.com>
2021-09-15 15:19:53 +0900

    編集Formおおまかに作った

--- TodoDiary/EditView.swift
+++ TodoDiary/EditView.swift
@@ -11,22 +11,39 @@ struct EditView: View {
     
     @Environment(\.presentationMode) var presentation
     
+    @State var content = ""
+    
     var body: some View {
         NavigationView {
-            Text("Hello")
-                .navigationBarTitle("Todoを編集", displayMode: .inline)
-                .navigationBarItems(
-                    leading: Button(action: {
-                        presentation.wrappedValue.dismiss()
-                    }){
-                        Text("キャンセル")
-                            .fontWeight(.regular)
-                    },
-                    trailing: Button("完了"){
-                        //Save Todo
-                        presentation.wrappedValue.dismiss()
+            
+            //Todo編集エリア
+            Form {
+                TextField("Todoを入力", text: $content)
+                
+                Section {
+                    Button("Todoを固定する"){
+                        //Todoを固定
                     }
-                )
+                    Button("Todoを削除"){
+                        //Todoを削除
+                    }
+                }
+            }
+            
+            //ナビゲーションバーの設定
+            .navigationBarTitle("Todoを編集", displayMode: .inline)
+            .navigationBarItems(
+                leading: Button(action: {
+                    presentation.wrappedValue.dismiss()
+                }){
+                    Text("キャンセル")
+                        .fontWeight(.regular)
+                },
+                trailing: Button("完了"){
+                    //Save Todo
+                    presentation.wrappedValue.dismiss()
+                }
+            )
         }
     }
 }
