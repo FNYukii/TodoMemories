e99ffa38e62ee27be92493daea7c95a95284fd42
Yu357 <forestgreen14689@gmail.com>
2021-09-16 09:52:32 +0900

    Todoモデルを定義した

---
+++ TodoDiary/Todo.swift
@@ -0,0 +1,26 @@
+//
+//  Todo.swift
+//  TodoDiary
+//
+//  Created by Yu on 2021/09/16.
+//
+
+import Foundation
+import RealmSwift
+
+class Todo: Object, Identifiable {
+    
+    //列を定義
+    @objc dynamic var id = 0
+    @objc dynamic var isPinned = false
+    @objc dynamic var isAchieved = false
+    @objc dynamic var achievedDate = Date()
+    @objc dynamic var content = ""
+    
+    //全てのレコードを返すメソッド
+    static func all() -> Results<Todo> {
+        let realm = try! Realm()
+        return realm.objects(Todo.self)
+    }
+    
+}
