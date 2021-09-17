326181d3deed014867a69cbdbc7d42a1905dc20d
Yu357 <forestgreen14689@gmail.com>
2021-09-17 22:38:19 +0900

    カレンダーの日付をタップすると画面遷移するようにした

---
+++ TodoDiary/ResultView.swift
@@ -0,0 +1,15 @@
+//
+//  ResultView.swift
+//  TodoDiary
+//
+//  Created by Yu on 2021/09/17.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        .navigationBarTitle("2021年 9日 15日")
+    }
+}
--- TodoDiary/ThirdView.swift
+++ TodoDiary/ThirdView.swift
@@ -11,11 +11,22 @@ struct ThirdView: View {
     
     @State var selectedDate = Date()
     
+    @State var isNavLinkActive = false
+    
     var body: some View {
         NavigationView {
             
-            CalendarView(selectedDate: $selectedDate)
-                                
+            VStack {
+                CalendarView(selectedDate: $selectedDate)
+                .onChange(of: selectedDate, perform: { value in
+                    isNavLinkActive = true
+                })
+                    
+                NavigationLink(destination: ResultView(), isActive: $isNavLinkActive) {
+                    EmptyView()
+                }
+            }
+            
             .navigationBarTitle("カレンダー")
         }
     }
