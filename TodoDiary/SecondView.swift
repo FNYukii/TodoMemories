ff2666e904a79ec0a48668d398885a558c50fdcc
Yu357 <forestgreen14689@gmail.com>
2021-09-18 17:57:46 +0900

    配列ymdsに基づいて日別の完了済みTodo一覧が表示されるようにした

--- TodoDiary/SecondView.swift
+++ TodoDiary/SecondView.swift
@@ -6,12 +6,15 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 struct SecondView: View, MyProtocol {
     
     @State var todos = Todo.achievedTodos()
     @State var isShowSheet = false
     @State var selectedTodoId = 0
+    
+    @State var ymds: [Int] = [20210916, 20210916, 20210917]
     
     var body: some View {
         NavigationView {
@@ -24,6 +27,19 @@ struct SecondView: View, MyProtocol {
                     }
                     .foregroundColor(.primary)
                 }
+                
+                ForEach(0..<ymds.count) { index in
+                    Section(header: Text("\(ymds[index])")) {
+                        ForEach(getDailyTodos(achievedYmd: ymds[index]).freeze()){ todo in
+                            Button("\(todo.content)"){
+                                selectedTodoId = todo.id
+                                isShowSheet.toggle()
+                            }
+                            .foregroundColor(.primary)
+                        }
+                    }
+                }
+                
             }
             .onAppear {
                 reloadRecords()
@@ -35,6 +51,11 @@ struct SecondView: View, MyProtocol {
             
             .navigationBarTitle("完了済み")
         }
+    }
+    
+    func getDailyTodos(achievedYmd: Int) -> Results<Todo> {
+        let realm = try! Realm()
+        return realm.objects(Todo.self).filter("isAchieved == true && achievedYmd == \(achievedYmd)").sorted(byKeyPath: "achievedDate", ascending: false)
     }
     
     func reloadRecords() {
