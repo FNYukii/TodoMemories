72a574afe3c88f5e2426cec720172ccbc18e09c9
Yu357 <forestgreen14689@gmail.com>
2021-09-19 19:11:35 +0900

    要らなくなった関数を削除

--- TodoDiary/CustomCalendarView.swift
+++ TodoDiary/CustomCalendarView.swift
@@ -14,8 +14,7 @@ struct CustomCalendarView: View {
     let showMonth: Int
     
     let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
-    @State var showDays: [Int] = [0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-    
+    @State var showDays: [Int] = []
     
     var body: some View {
         VStack {
@@ -57,21 +56,6 @@ struct CustomCalendarView: View {
     func today() -> Int {
         let calendar = Calendar(identifier: .gregorian)
         return calendar.component(.day, from: Date())
-    }
-    
-    //当月の一日の曜日
-    func firstWeekday(year:Int,month:Int,day:Int) -> Int{
-        var result : Int = 0
-        if month == 1 || month ==  2 {
-            var changeyear : Int = year
-            var changemonth : Int = month
-            changeyear -= 1
-            changemonth += 12
-            result  = (day + (26 * (changemonth + 1))/10 + changeyear + (changeyear / 4) + (5 * (changeyear / 100)) + ((changeyear / 100)/4) + 5) % 7 as Int
-        }else{
-            result  = (day + (26 * (month + 1))/10 + year + (year / 4) + (5 * (year / 100)) + ((year / 100)/4) + 5) % 7 as Int
-        }
-        return result
     }
     
     //当月の日の配列 例: [0, 0, 0, 1, 2, 3, ...]
