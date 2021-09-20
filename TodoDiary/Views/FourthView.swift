e5ba20a0d9f053b35a05e10207a34bb80b56460b
Yu357 <forestgreen14689@gmail.com>
2021-09-20 11:47:48 +0900

    試行錯誤中

--- TodoDiary/Views/FourthView.swift
+++ TodoDiary/Views/FourthView.swift
@@ -11,14 +11,37 @@ import SwiftUI
 
 struct FourthView: View {
     
-    @State private var currentPage = 0
+    //表示中のページ番号
+    @State var currentPage = 0 {
+        didSet {
+            print("\(oldValue) -> \(currentPage)")
+            if currentPage == 1 && oldValue == 0 || currentPage == 2 && oldValue == 1 || currentPage == 0 && oldValue == 2 {
+                print("up")
+            } else {
+                print("down")
+            }
+        }
+    }
+    
+    
+    @State var offset = 0
     
     var body: some View {
-        PageView([
-            AnyView(Page1(offset: 3)),
-            AnyView(Page2(offset: 5)),
-            AnyView(Page3(offset: 7))
-        ], currentPage: $currentPage)
+        
+        VStack {
+            
+            PageView([
+                AnyView(Page1(offset: 3)),
+                AnyView(Page2(offset: 5)),
+                AnyView(Page3(offset: 7))
+            ], currentPage: $currentPage)
+            
+//            Text("\(currentPage)")
+            
+        }
+        
+        
+        
     }
 }
 
