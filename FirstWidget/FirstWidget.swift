6b5c97d7701eadaaff67e5d5fb9b586effa88908
Yu357 <forestgreen14689@gmail.com>
2021-09-20 22:38:12 +0900

    Widget用のデータに、固定済みTodoと未固定Todoを区別してセットした

--- FirstWidget/FirstWidget.swift
+++ FirstWidget/FirstWidget.swift
@@ -11,32 +11,33 @@ import Intents
 
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), todoStrs: ["Apple", "Orange"])
+        SimpleEntry(date: Date(), pinnedTodoStrs: ["Apple", "Orange"], unpinnedTodoStrs: ["Strawberry", "melon"])
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), todoStrs: ["Strawberry", "Melon"])
+        let entry = SimpleEntry(date: Date(), pinnedTodoStrs: ["Apple", "Orange"], unpinnedTodoStrs: ["Strawberry", "melon"])
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        
-        //全てのTodoを取得
-        var todoStrs: [String] = []
-        let todos = Todo.unachievedTodos()
-        print(todos.count)
-        for todo in todos {
-            todoStrs.append(todo.content)
+                
+        //固定済みTodoの文字列型配列を生成
+        var pinnedTodoStrs: [String] = []
+        let pinnedTodos = Todo.pinnedTodos()
+        for pinnedTodo in pinnedTodos {
+            pinnedTodoStrs.append(pinnedTodo.content)
         }
         
-        
-        
-        
-        
+        //未固定Todoの文字列型配列を生成
+        var unpinnedTodoStrs: [String] = []
+        let unpinnedTodos = Todo.unpinnedTodos()
+        for unpinnedTodo in unpinnedTodos {
+            unpinnedTodoStrs.append(unpinnedTodo.content)
+        }
         
         //Entryにデータをセット
         var entries: [SimpleEntry] = []
-        let entry = SimpleEntry(date: Date(), todoStrs: todoStrs)
+        let entry = SimpleEntry(date: Date(), pinnedTodoStrs: pinnedTodoStrs, unpinnedTodoStrs: unpinnedTodoStrs)
         entries.append(entry)
 
         let timeline = Timeline(entries: entries, policy: .atEnd)
@@ -46,7 +47,8 @@ struct Provider: IntentTimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let todoStrs: [String]
+    let pinnedTodoStrs: [String]
+    let unpinnedTodoStrs: [String]
 }
 
 struct FirstWidgetEntryView : View {
@@ -54,8 +56,11 @@ struct FirstWidgetEntryView : View {
 
     var body: some View {
         VStack {
-            ForEach(0..<entry.todoStrs.count) { index in
-                Text("\(entry.todoStrs[index])")
+            ForEach(0..<entry.pinnedTodoStrs.count) { index in
+                Text("\(entry.pinnedTodoStrs[index])")
+            }
+            ForEach(0..<entry.unpinnedTodoStrs.count) { index in
+                Text("\(entry.unpinnedTodoStrs[index])")
             }
             Spacer()
         }
