32d7fdf2ee50bc7aa369aa3cb88266a2ca55b933
Yu357 <forestgreen14689@gmail.com>
2021-11-24 23:31:58 +0900

    プロジェクトをSwiftUI Appへ移行

---
+++ TodoDiary/TodoDiaryApp.swift
@@ -0,0 +1,18 @@
+//
+//  TodoDiaryApp.swift
+//  TodoDiary
+//
+//  Created by Yu on 2021/11/24.
+//
+
+import Foundation
+import SwiftUI
+
+@main
+struct TodoDiaryApp: App {
+   var body: some Scene {
+       WindowGroup {
+           ContentView()
+       }
+   }
+}
