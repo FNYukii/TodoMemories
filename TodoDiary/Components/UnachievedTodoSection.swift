465d9cf70f78eeaba1a0e01e14e0d829568dd8fc
Yu357 <forestgreen14689@gmail.com>
2021-11-26 09:28:43 +0900

    コード量を削減

--- TodoDiary/Components/UnachievedTodoSection.swift
+++ TodoDiary/Components/UnachievedTodoSection.swift
@@ -22,63 +22,32 @@ struct UnachievedTodoSection: View {
     @Environment(\.editMode) var editMode
     
     var body: some View {
-        if !headerText.isEmpty {
-            Section(header: Text(headerText)) {
-                ForEach(todos.freeze()) { todo in
-                    Button(action: {
-                        selectedTodoId = todo.id
-                        isShowSheet.toggle()
-                    }) {
-                        HStack {
-                            Text("\(todo.order)")
-                                .foregroundColor(.secondary)
-                            Text(todo.content)
-                        }
-                    }
-                    .foregroundColor(.primary)
-                    .contextMenu {
-                        UnachievedTodoContextMenuItems(editProtocol: editProtocol, todoId: todo.id, isPinned: todo.isPinned, isAchieved: todo.isAchieved, isShowActionSheet: $isShowActionSheet, selectedTodoId: $selectedTodoId)
+        Section(header: !headerText.isEmpty ? Text(headerText) : Text("")) {
+            ForEach(todos.freeze()) { todo in
+                Button(action: {
+                    selectedTodoId = todo.id
+                    isShowSheet.toggle()
+                }) {
+                    HStack {
+                        Text("\(todo.order)")
+                            .foregroundColor(.secondary)
+                        Text(todo.content)
                     }
                 }
-                .onMove {sourceIndexSet, destination in
-                    Todo.sortTodos(todos: todos, sourceIndexSet: sourceIndexSet, destination: destination)
-                    editProtocol.reloadTodos()
-                }
-                .onDelete {indexSet in
-                    indexSet.sorted(by: > ).forEach { (i) in
-                        selectedTodoId = todos[i].id
-                    }
-                    isShowActionSheet.toggle()
+                .foregroundColor(.primary)
+                .contextMenu {
+                    UnachievedTodoContextMenuItems(editProtocol: editProtocol, todoId: todo.id, isPinned: todo.isPinned, isAchieved: todo.isAchieved, isShowActionSheet: $isShowActionSheet, selectedTodoId: $selectedTodoId)
                 }
             }
-        } else {
-            Section {
-                ForEach(todos.freeze()) { todo in
-                    Button(action: {
-                        selectedTodoId = todo.id
-                        isShowSheet.toggle()
-                    }) {
-                        HStack {
-                            Text("\(todo.order)")
-                                .foregroundColor(.secondary)
-                            Text(todo.content)
-                        }
-                    }
-                    .foregroundColor(.primary)
-                    .contextMenu {
-                        UnachievedTodoContextMenuItems(editProtocol: editProtocol, todoId: todo.id, isPinned: todo.isPinned, isAchieved: todo.isAchieved, isShowActionSheet: $isShowActionSheet, selectedTodoId: $selectedTodoId)
-                    }
+            .onMove {sourceIndexSet, destination in
+                Todo.sortTodos(todos: todos, sourceIndexSet: sourceIndexSet, destination: destination)
+                editProtocol.reloadTodos()
+            }
+            .onDelete {indexSet in
+                indexSet.sorted(by: > ).forEach { (i) in
+                    selectedTodoId = todos[i].id
                 }
-                .onMove {sourceIndexSet, destination in
-                    Todo.sortTodos(todos: todos, sourceIndexSet: sourceIndexSet, destination: destination)
-                    editProtocol.reloadTodos()
-                }
-                .onDelete {indexSet in
-                    indexSet.sorted(by: > ).forEach { (i) in
-                        selectedTodoId = todos[i].id
-                    }
-                    isShowActionSheet.toggle()
-                }
+                isShowActionSheet.toggle()
             }
         }
     }
