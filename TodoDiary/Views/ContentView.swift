8af2baa8ef0ff211aeb00fbd5e3c56e948c104a3
Yu357 <forestgreen14689@gmail.com>
2021-11-27 09:42:38 +0900

    TabViewのselectionの変更を検知できた

--- TodoDiary/Views/ContentView.swift
+++ TodoDiary/Views/ContentView.swift
@@ -8,20 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selection = 0
+    
+    var handler: Binding<Int> { Binding(
+        get: { self.selection },
+        set: {
+            print("tab\($0) selected")
+            if $0 == self.selection {
+                print("reload view")
+            }
+            self.selection = $0
+        }
+    )}
+    
     var body: some View {
-        TabView {
+        TabView(selection: handler) {
             FirstView()
                 .tabItem {
                     Label("Todo", systemImage: "list.bullet")
                 }
+                .tag(0)
             SecondView()
                 .tabItem {
                     Label("達成済み", systemImage: "checkmark")
                 }
+                .tag(1)
             ThirdView()
                 .tabItem {
                     Label("達成グラフ", systemImage: "chart.xyaxis.line")
                 }
+                .tag(2)
         }
         .accentColor(.red)
     }
