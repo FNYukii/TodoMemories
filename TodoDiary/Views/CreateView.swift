6444a020b25bbed9ee5382d36b3335503fc7128b
Yu357 <forestgreen14689@gmail.com>
2022-02-12 01:21:34 +0900

    new file

---
+++ TodoDiary/Views/CreateView.swift
@@ -0,0 +1,14 @@
+//
+//  CreateView.swift
+//  TodoDiary
+//
+//  Created by Yu on 2022/02/12.
+//
+
+import SwiftUI
+
+struct CreateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
