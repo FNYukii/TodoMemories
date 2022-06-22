0ca3fa9a459084d3fed46f8e3b58a920aa81f9ed
Yu357 <forestgreen14689@gmail.com>
2022-06-22 11:55:39 +0900

    feat: AchievedDaysViewModelクラスをおおまかに作成

---
+++ TodoMemories/ViewModels/AchievedDaysViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  AchievedDaysViewModel.swift
+//  TodoMemories
+//
+//  Created by Yu on 2022/06/22.
+//
+
+import Foundation
+import RealmSwift
+
+class AchievedDaysViewModel: ObservableObject {
+    
+    @Published var days: [Day] = []
+    @Published var isLoaded = false
+    
+    var token: NotificationToken? = nil
+    
+    init() {
+        loadDays()
+        
+        // Realmデータベースに変更があった場合
+        let realm = Todo.customRealm()
+        token = realm.observe { (notification, realm) in
+            self.loadDays()
+        }
+    }
+    
+    private func loadDays() {
+        
+        // 全ての達成済みTodoを取得
+        
+        // 配列daysを生成
+        
+        // プロパティに格納
+        
+    }
+    
+    deinit {
+        token?.invalidate()
+    }
+    
+}
