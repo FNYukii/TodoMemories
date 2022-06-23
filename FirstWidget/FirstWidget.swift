2405cbe00bc3c95685f3a5536a8427a797c20521
Yu357 <forestgreen14689@gmail.com>
2022-06-23 11:51:10 +0900

    feat: WidgetのUIを刷新

--- FirstWidget/FirstWidget.swift
+++ FirstWidget/FirstWidget.swift
@@ -11,35 +11,35 @@ import Intents
 
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), todoContents: ["買い物に行く", "宿題をする"], isPinned: false)
+        // Todosを取得
+        var todos: [Todo] = []
+        todos.append(contentsOf: Array(Todo.pinnedTodos()))
+        todos.append(contentsOf: Array(Todo.unpinnedTodos()))
+        
+        return SimpleEntry(date: Date(), todos: todos)
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), todoContents: ["買い物に行く", "宿題をする"], isPinned: false)
+        // Todosを取得
+        var todos: [Todo] = []
+        todos.append(contentsOf: Array(Todo.pinnedTodos()))
+        todos.append(contentsOf: Array(Todo.unpinnedTodos()))
+        
+        // Entryを生成
+        let entry = SimpleEntry(date: Date(), todos: todos)
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
                 
-        let pinnedTodos = Todo.pinnedTodos()
-        let unpinnedTodos = Todo.unpinnedTodos()
-        var todos = Todo.noRecord()
-        var isPinned = false
-        if pinnedTodos.count > 0 {
-            todos = pinnedTodos
-            isPinned = true
-        } else {
-            todos = unpinnedTodos
-        }
-        
-        var todoContents: [String] = []
-        for todo in todos {
-            todoContents.append(todo.content)
-        }
-        
+        // Todosを取得
+        var todos: [Todo] = []
+        todos.append(contentsOf: Array(Todo.pinnedTodos()))
+        todos.append(contentsOf: Array(Todo.unpinnedTodos()))
+                
         //Entryにデータをセット
         var entries: [SimpleEntry] = []
-        let entry = SimpleEntry(date: Date(), todoContents: todoContents, isPinned: isPinned)
+        let entry = SimpleEntry(date: Date(), todos: todos)
         entries.append(entry)
 
         let timeline = Timeline(entries: entries, policy: .atEnd)
@@ -49,8 +49,7 @@ struct Provider: IntentTimelineProvider {
 
 struct SimpleEntry: TimelineEntry {
     let date: Date
-    let todoContents: [String]
-    let isPinned: Bool
+    let todos: [Todo]
 }
 
 struct FirstWidgetEntryView : View {
@@ -61,64 +60,46 @@ struct FirstWidgetEntryView : View {
     //Todoのデータ
     var entry: Provider.Entry
     
-    //4 or 12
-    var maxItemCount: Int {
-        switch self.widgetFamily {
-            case .systemSmall: return 4
-            case .systemMedium: return 4
-            case .systemLarge: return 12
-            case .systemExtraLarge: return 12
-            default: return 4
-        }
-    }
+//    //4 or 12
+//    var maxItemCount: Int {
+//        switch self.widgetFamily {
+//            case .systemSmall: return 4
+//            case .systemMedium: return 4
+//            case .systemLarge: return 12
+//            case .systemExtraLarge: return 12
+//            default: return 4
+//        }
+//    }
     
-    var showItemCount: Int {
-        if entry.todoContents.count > maxItemCount {
-            return maxItemCount
-        } else {
-            return entry.todoContents.count
-        }
-    }
+//    var showItemCount: Int {
+//        if entry.todoContents.count > maxItemCount {
+//            return maxItemCount
+//        } else {
+//            return entry.todoContents.count
+//        }
+//    }
 
     var body: some View {
-        ZStack {
+        VStack(alignment: .leading) {
             
-            Color("background")
-            
-            VStack(alignment: .leading) {
-                
-                //ラベル
-                ZStack(alignment: .leading) {
-                    Color.red
-                        .frame(height: 25)
-                    Text(entry.isPinned ? "固定済み" : "Todo")
-                        .font(.subheadline)
-                        .fontWeight(.bold)
-                        .foregroundColor(Color.white)
-                        .padding(.leading)
-                }
-                
-                //Todo一覧
-                ForEach(0..<showItemCount) { index in
-                    Text("\(entry.todoContents[index])")
-                        .font(.subheadline)
-                        .frame(height: 23)
-                        .padding(.leading)
-                }
-                
-                if entry.todoContents.count > maxItemCount {
-                    Text("\(entry.todoContents.count - maxItemCount) More")
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                        .frame(height: 23)
-                        .padding(.leading)
-                }
-                
-                Spacer()
-                
+            // Todos
+            ForEach(entry.todos) { todo in
+                Text(todo.content)
+                    .font(.subheadline)
+                    .fontWeight(todo.isPinned ? .bold : .regular)
+                    .lineLimit(1)
             }
             
+//                if entry.todoContents.count > maxItemCount {
+//                    Text("\(entry.todoContents.count - maxItemCount) More")
+//                        .font(.caption)
+//                        .foregroundColor(.secondary)
+//                        .frame(height: 23)
+//                        .padding(.leading)
+//                }
+            Spacer()
         }
+        .padding(6)
     }
 }
 
